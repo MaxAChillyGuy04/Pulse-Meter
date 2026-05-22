473b609d48ebc59eb9fc42ddbd7332f3d9a6ad94
MaxAChillyGuy04 <kittitatgromkor@gmail.com>
2026-05-22 23:35:58 +0700

    no more that folder, moved everything to top level. and added README

---
+++ mainScript.js
@@ -0,0 +1,10 @@
+import PulsoidSocket from '@pulsoid/socket';
+
+const socket = PulsoidSocket.create('77ccc9d3-3a67-485b-a8bb-34095c2ad8f6');
+
+socket.on('heart-rate', (data) => {
+    console.log('${data.heartRate} BPM');
+    document.getElementsByClassName('value-text').innerHTML = '${data.heartRate}';
+});
+
+await socket.connect();
